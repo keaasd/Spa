ec696e811c277a474c18a5c7f94ae2413f4f5d2e
“Евгений <kea-asd@yandex.ru>
2022-04-05 01:55:29 +0700

    Input

--- js/swiper.js
+++ js/swiper.js
@@ -20,10 +20,10 @@ var swiper = new Swiper(".mySwiper", {
   slidesPerGroup: 1,
   spaceBetween: 319,
   loop: true,
-  autoplay: {
-          delay: 2500,
-          disableOnInteraction: false,
-        },
+  // autoplay: {
+  //         delay: 2500,
+  //         disableOnInteraction: false,
+  //       },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
