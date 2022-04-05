8090819d89137b15a2f89ef3e455452fffff8890
“Евгений <kea-asd@yandex.ru>
2022-04-05 22:38:32 +0700

    slider2

--- js/swiper.js
+++ js/swiper.js
@@ -3,32 +3,50 @@ var swiper = new Swiper(".mySwiper", {
   // autoplay: {
   //   delay: 2500,
   //   disableOnInteraction: false,
-    // таймер выше  
+  // таймер выше  
   pagination: {
-      el: ".swiper-pagination",
-      type: "progressbar",
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 // slider 2 services
 {
   var swiper = new Swiper(".swiper_services", {
-  slidesPerView: 3,
-  slidesPerGroup: 1,
-  spaceBetween: 319,
-  loop: true,
-  // autoplay: {
-  //         delay: 2500,
-  //         disableOnInteraction: false,
-  //       },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
+    slidesPerView: 2.5,
+    slidesPerGroup: 1,
+    // spaceBetween: 20,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      300: {
+        slidesPerView: 2.5,
+        slidesPerGroup: 1,
+        spaceBetween: 60,
+      },
+      360: {
+        slidesPerView: 2.5,
+        slidesPerGroup: 1,
+        spaceBetween: 60,
+      },
+      1200: {
+        slidesPerView: 2.5,
+        slidesPerGroup: 1,
+        spaceBetween: 20,
+      },
+      1440: {
+        slidesPerView: 2.5,
+        slidesPerGroup: 1,
+        spaceBetween: 114,
+      },
+    }
+  });
 }
 
 
