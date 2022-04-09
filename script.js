fb4ee66faa66e69e89402101d73660886afedfc7
“Евгений <kea-asd@yandex.ru>
2022-04-09 16:16:41 +0700

    import

--- script.js
+++ script.js
@@ -22,44 +22,42 @@ searchBtn.addEventListener('click', (e) => {
 });
 
 
-  // Initialize Swiper3
- 
-  $(document).ready(function(){
-    $('.slider3').slick({
-      arrows:true,
-      dots:false,
-      fade:false,
-      adaptiveHeight:true,
-      slidesToShow:2,
-      slidesToScroll:1,
-      speed:500,
-      easing:'ease',
-      infinite:true,
-      initialSlide:0,
-      autoplay:false,
-      autoplaySpeed:2000,
-      pauseOnFocus:true,
-      pauseOnHover:true,
-      pauseOnDotsHover:true,
-      draggable:true,
-      swipe:true,
-      touchThreshold:10,
-      touchMove:true,
-      variableWidth:true,
-      centerMode:false,
-      // centerPadding: '60px',
-      waitForAnimate:false,
-      responsive:[
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow:1
-          }
-        }
-      ]
-    });
-  
+ // третий свайпер
+{
+
+  var swiper = new Swiper(".gallerySwiper", {
+    slidesPerView: 2,
+    loop: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "fraction",
+    },
+    breakpoints: {
+      280: {
+        spaceBetween: 20,
+      },
+      360: {
+        spaceBetween: 40,
+      },
+      567: {
+        spaceBetween: 40,
+      },
+      1040: {
+        spaceBetween: 50,
+      },
+      1200: {
+        spaceBetween: 63,
+      },
+      1360: {
+        spaceBetween: 63,
+      },
+    }
   });
+}
 
   function openModal() {
     document.getElementById("modal__mb").style.left = "0px";
